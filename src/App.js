a38d120ebcf595b59fa54b23def8242e5f7ed64d
FlavioRbs1 <flaviorbs3@gmail.com>
2022-03-11 01:02:42 -0300

    Eventos Criados

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import Item from './components/item/index'
+import Card from './components/Card/index'
 
 function App() {
   return (
-    <h1>Hello Word in React</h1>
+    <>
+      <h1>Minha primeira aplicação em react</h1>
+      <ul>       
+        <Item>Item 1</Item>  
+        <Item>Item 2</Item>  
+        <Item>Item 3</Item>   
+      </ul>
+      <Card />
+    </>
   );
 }
 
---
+++ src/components/Card/index.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+const Card = () =>{
+
+    const [valor, setValor] = useState(0)
+
+    function Adicionar(){
+        setValor(valor + 1)
+    }
+
+    function Subtrair(){
+        setValor(valor - 1)
+    }
+
+    return(
+        <div className="card">
+            <h5 className="card-header">Meu Primeiro Card</h5>
+            <div className="card-body">
+                <button 
+                    type="button" 
+                    className="btn btn-success"
+                    onClick={Adicionar}>
+                        Adicionar
+                </button>
+                <button 
+                    type="button" 
+                    className="btn btn-danger"
+                    onClick={Subtrair}>
+                        Remover
+                </button>
+                <p>{valor}</p>
+            </div>
+         </div>
+    )
+}
+export default Card;
